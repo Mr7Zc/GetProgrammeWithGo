868d7dfeff081e07408187cfa99789411c111627
Mr7z <2438171792@qq.com>
2020-12-13 00:45:52 +0800

    1-2
    格式化输出

---
+++ 1-2/fmt.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("My weight is %v lbs", 149.0*0.3783)
+	fmt.Printf(" and I would be %v yearsd old. \n", 41*364/687)
+}
---
+++ 1-2/mars.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("My weight on the surface of Mars is ")
+	fmt.Println(149.0 * 0.3783)
+	fmt.Println(" lbs,and I would be ")
+	fmt.Println(41 * 364 / 687)
+	fmt.Println(" years old.")
+}
